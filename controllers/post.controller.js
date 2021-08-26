b4cb5d8c9fbe3a9c2501ed1535ddc44861d27441
Alven86 <alvenj@yahoo.com>
2021-08-26 11:49:09 +0200

    CRUD and Validation

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -1,3 +1,4 @@
+const Validator = require('fastest-validator');
 const models = require('../models');
 function save(req, res){
     const post = {
@@ -8,6 +9,21 @@ function save(req, res){
         userId: 1
     }
 
+    const schema = {
+        title:{type: "string", optional: false, max: "100"},
+        content:{type: "string", optional: false, max: "500"},
+        categoryId:{type: "number", optional: false}
+    }
+
+    const v = new Validator();
+    const validationResponse = v.validate(post, schema);
+
+    if (validationResponse !== true){
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: validationResponse
+        });
+    }
 
     models.Post.create(post).then(result => {
       res.status(201).json({
@@ -22,6 +38,104 @@ function save(req, res){
     }); 
 }
 
+function show (req, res){
+    const id = req.params.id;
+
+    models.Post.findByPk(id).then(result =>{
+        if(result){
+            res.status(200).json(result);
+        }else{
+            res.status(404).json({
+                message: "Post not found!"
+            })
+        }
+    }).catch(error =>{
+        res.status(500).json({
+            message:"something went wrong"
+        })
+    });
+}
+
+function index(req, res){
+    models.Post.findAll().then(result =>{
+        res.status(200).json(result);
+
+    }).catch(error =>{
+        res.status(500).json({
+            message:"something went wrong"
+        })
+
+    });
+}
+
+
+function update (req, res){
+    const id = req.params.id;
+    const updatedPost = {
+        title: req.body.title,
+        content: req.body.content,
+        imageUrl: req.body.image_url,
+        categoryId: req.body.category_id
+    }
+
+    const userId = 1;
+
+    const schema = {
+        title:{type: "string", optional: false, max: "100"},
+        content:{type: "string", optional: false, max: "500"},
+        categoryId:{type: "number", optional: false}
+    }
+
+    const v = new Validator();
+    const validationResponse = v.validate(updatedPost, schema);
+
+    if (validationResponse !== true){
+        return res.status(400).json({
+            message: "Validation failed",
+            errors: validationResponse
+        });
+    }
+
+    models.Post.update(updatedPost,{where:{id:id, userId:userId}}).then(result =>{
+             res.status(200).json({
+                 message: "Post updated successfully",
+                 post: updatedPost
+                });
+         
+    }).catch(error =>{
+        res.status(500).json({
+            message:"something went wrong",
+            error: error
+        })
+    });
+}
+
+function destroy(req, res){
+    const id = req.params.id;
+    const userId = 1;
+
+    models.Post.destroy({where:{id:id, userId:userId}}).then(result =>{
+        if(result){
+             res.status(200).json({
+                 message: "Post deleted successfully"
+             });
+        }else{
+            res.status(404).json({
+                 message: "Post not found!"
+             })
+        }
+
+    }).catch(error =>{
+        res.status(500).json({
+            message:"something went wrong",
+            error: error
+        });
+    });
+}
     module.exports = {
-    save: save
+    save: save,
+    show: show,
+    index: index,
+    update: update,
+    destroy: destroy
 }
